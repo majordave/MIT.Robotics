638c337591cafb72b6ef28083a3368cc470052b3
Dave Mayorz <davemayorz94@gmail.com>
2017-02-20 23:40:12 -0600

    Plot and organization.

---
+++ test2.py
@@ -0,0 +1,12 @@
+from kine import *
+import csv
+# Testing forward kinematics validation method
+
+d1 = 37.25
+a1 = 138.44
+a2 = 35.65
+theta = csv.reader(open('theta.csv'), delimiter=',')
+pos = csv.reader(open('pos.csv'), delimiter=',')
+theta = Matrix(list(theta))
+pos = Matrix(list(pos))
+validate(theta, pos, 'd', d1, a1, a2)
