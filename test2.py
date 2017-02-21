63a19e0ffcdd4e4524ae9f10647a9b998233d4f6
Dave Mayorz <davemayorz94@gmail.com>
2017-02-21 16:35:06 -0600

    Final Validation Commit

--- test2.py
+++ test2.py
@@ -3,8 +3,9 @@ import csv
 # Testing forward kinematics validation method
 
 d1 = 37.25
-a1 = 139.29
+a1 = 110.32
 a2 = 35.65
+print(d1 + a1 + a2)
 theta = csv.reader(open('theta.csv'), delimiter=',')
 pos = csv.reader(open('pos.csv'), delimiter=',')
 theta = Matrix(list(theta))
