251b901dfdec67aae8226951cca7ad9af5100e09
Dave Mayorz <davemayorz94@gmail.com>
2017-03-13 18:14:21 -0600

    Sped up th1 solver

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 from kine import MatPos, Th0, Th1, Th2
-from sympy import pprint, pi
+from sympy import pi, pprint
 
 # Testing simple forward and inverse kinematics
 
@@ -9,8 +9,8 @@ a2 = 10
 test = MatPos(0, -pi/2, 0, d1, a1, a2)
 pprint(test)
 th0 = Th0(test)
-th1 = Th1(test, th0, d1, a2)
-th2 = Th2(test, th0, th1, d1, a2)
 print("th0 = ", th0)
+th1 = Th1(test, th0, d1, a2)
 print("th1 = ", th1)
+th2 = Th2(test, th1, d1, a2)
 print("th2 = ", th2)
